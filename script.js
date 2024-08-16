89d75cbbc4c6182998555838a4c044edacafd1d7
Wixda <Iamwixda@gmail.com>
2024-08-17 00:43:09 +0100

    restyle the password container

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function getRandomChar () {
 
 function generatePassword() {
     let password = "";
-    for (let i = 0; i < 8; i++) {
+    for (let i = 0; i < 15; i++) {
         password += getRandomChar()
     }
     return password;
